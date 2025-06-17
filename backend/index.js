d9aade9b447147cdb715018a0b275be28b0a0e43
Kier Joshua <kierjoshuadu1226@gmail.com>
2025-06-17 19:23:15 +0800

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 connectDB()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://netflix-wfjl.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
